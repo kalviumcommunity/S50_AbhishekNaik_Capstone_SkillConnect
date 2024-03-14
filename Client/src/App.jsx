21fbf9c318f75d66ba2c9f47762ee5eb92b539a8
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2024-03-14 16:53:39 +0530

    updated code with bot suggestions

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "ldrs/trio";
+import LoadingSpinner from "./utils/Loader";
 
 const LandingPage = lazy(() => import("./components/Landing Page/LandingPage"));
 const SignUpPage = lazy(() => import("./components/Sign Up/SignupPage"));
@@ -10,13 +10,7 @@ const About = lazy(() => import("./components/About Page/About"));
 const App = () => {
   return (
     <Router>
-      <Suspense
-        fallback={
-          <div className="flex justify-center items-center h-screen">
-            <l-trio size="50" speed="1.5" color="black"></l-trio>
-          </div>
-        }
-      >
+      <Suspense fallback={<LoadingSpinner />}>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/signup" element={<SignUpPage />} />
--- Server/server.js
+++ Server/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use("/ping", pingRoute);
 app.use("/user", userRoute);
-app.use(cors())
+app.use(cors());
 DatabaseConnection();
 
 const server = app.listen(port, () => {
