efc4db2b869a164bf66e75d49ebf2a3297517abc
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2024-03-15 11:50:39 +0530

    Added Signup(Frontend),Login(Backend) and Homepage

--- Server/models/User.js
+++ Server/models/User.js
@@ -12,10 +12,6 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
-    },
-    googleId: {
-        type: String,
-        required: false
     }
 });
 
--- Server/routes/user.js
+++ Server/routes/user.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const { getAllUsers, getUserById, createUser, deleteUserById, updateUserById } = require("../controllers/userControllers");
+const { getAllUsers, getUserById, createUser, deleteUserById, updateUserById, loginUser } = require("../controllers/userControllers");
 const validateSignupUserData = require("../middleware/validateSignupUserData");
 const validateEditUserData = require("../middleware/validateEditUserData");
+const validateLoginUserData = require("../middleware/validateLoginUserData");
 
 router.get("/", getAllUsers);
 router.get("/:id", getUserById);
+
+
 router.post("/", validateSignupUserData, createUser);
-router.delete("/:id",  deleteUserById);
+router.post("/login", validateLoginUserData, loginUser);
+
 router.put("/:id", validateEditUserData, updateUserById);
+
 router.patch("/:id", validateEditUserData, updateUserById);
+
+router.delete("/:id",  deleteUserById);
 
 module.exports = router;
